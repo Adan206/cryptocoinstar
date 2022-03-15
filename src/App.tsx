b0a76bc0e70049fb17b390f0be14da65a53fc792
Adan Abbi <adanweli@hotmail.com>
2022-03-14 23:34:47 -0700

    new

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path='/cryptocoinstar' element={<Coinlist />} />
-          <Route path='/coin/:id' element={<CoinPage />} />
+          <Route path='cryptocoinstar/' element={<Coinlist />} />
+          <Route path='coin/:id' element={<CoinPage />} />
           {/* <Route path='/coin/:id' element={<CoinPage />} /> */}
           {/* <Route path='*' element={<Navigate to='/cryptocoinstar' />} /> */}
         </Routes>
